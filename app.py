72b534cfb0698c3b4a3b96005d6c67902b0021bc
Wang Rongfei <xrowii@outlook.com>
2024-05-03 00:27:28 +0800

    chore: format `app.py`

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 from huh import app
 import os
 
-HOST = os.getenv('HUH_HOST') or '0.0.0.0'
-PORT = int(os.getenv('HUH_PORT') or 5000)
+HOST = os.getenv("HUH_HOST") or "0.0.0.0"
+PORT = int(os.getenv("HUH_PORT") or 5000)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(host=HOST, port=PORT, debug=True)
