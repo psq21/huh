dfbe3481fba9e9329fa4349df01172e32f8235cc
PSQ <psq2114@gmail.com>
2024-05-05 12:08:29 +0800

    added db methods and modified templates for announcements

--- huh/announcement.py
+++ huh/announcement.py
@@ -2,21 +2,25 @@ from flask import Blueprint, render_template, request
 import sqlite3
 
 from huh import db
+from db import * 
 
 bp = Blueprint("announcement", __name__, url_prefix="/announcement")
 
 
-@bp.route('/all', methods=['GET','POST'])
+@bp.route('/all/', methods=['GET','POST'])
 def allAnn():
     if request.method=='GET': #return page of all announcements
-        conn = sqlite3.connect('app.db')
-        conn.row_factory=sqlite3.Row
-        cursor = conn.execute("""SELECT Announcement.rowid,title,name,timestamp,content FROM 
-                              Announcement JOIN User ON Announcement.author_id=User.rowid""")
-        data = cursor.fetchall()
+        conn = connect()
+        data = Entry.all_ann(conn)
         conn.close()
         return render_template('allAnn.html',anns=data)
     
-@bp.route('/<annID>')
+@bp.route('/<annID>/', methods=['GET','POST'])
 def oneAnn(annID):
-    return render_template('oneAnn.html',)
+    if request.method=='GET': #return page of one announcement
+        conn = connect()
+        ann = Entry.one_ann(conn,annID)
+        comments = Entry.one_ann_comments(conn,annID)
+        attachments = Entry.one_ann_attachments(conn,annID)
+        conn.close()
+    return render_template('oneAnn.html',ann=ann,comments=comments,attachments=attachments)
