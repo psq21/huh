be19ae42cdb507043cd049ce810cab516c68e18c
PSQ <psq2114@gmail.com>
2024-05-06 17:24:32 +0800

    changed things relating to comment component

--- huh/announcement.py
+++ huh/announcement.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, render_template, request
+from flask_login import login_required, current_user
 import sqlite3
 
 from huh import db
@@ -14,13 +15,19 @@ def allAnn():
         data = Announcement.all_ann_w_name(conn)
         conn.close()
         return render_template('allAnn.html',anns=data)
-    
+
+@login_required
 @bp.route('/<annID>/', methods=['GET','POST'])
 def oneAnn(annID):
     if request.method=='GET': #return page of one announcement
         conn = connect()
         ann = Announcement.one_ann(conn,annID)
-        comments = Announcement.one_ann_comments(conn,annID)
         attachments = Announcement.one_ann_attachments(conn,annID)
+        comments = Announcement.one_ann_comments(conn,annID)
+        for comm in comments:
+            if comm['author_id']==current_user.id:
+                comm['allowed_edit'] = True 
+            else:
+                comm['allowed_edit'] = False
         conn.close()
     return render_template('oneAnn.html',ann=ann,comments=comments,attachments=attachments)
