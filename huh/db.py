8de2becf6b44c6382fd675b6b5bc7ab5b4b88837
PSQ <psq2114@gmail.com>
2024-05-09 18:32:20 +0800

    added: some shitty styles

--- huh/db.py
+++ huh/db.py
@@ -200,7 +200,7 @@ class Announcement(Entry):
         cur.row_factory = sqlite3.Row
         res = cur.execute(
             """
-            SELECT Announcement.rowid,title,timestamp,content, User.name
+            SELECT Announcement.rowid as annID,title,timestamp,content, User.name
             FROM Announcement JOIN User ON Announcement.author_id=User.rowid
             WHERE Announcement.rowid=?
             """,
